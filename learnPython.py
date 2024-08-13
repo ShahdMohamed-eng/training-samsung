9e95ff5fb069912f89710e4af42e5cf92c5a9ccb
ShahdMohamed-eng <shahodam720@gmail.com>
2024-08-13 13:20:48 +0300

    add

--- learnPython.py
+++ learnPython.py
@@ -1,1 +1,13 @@
+# This function will print the average of 3 numbers
 
+def average():
+    # Prompt the user to enter three numbers
+    numbers = input("Enter three numbers separated by spaces: ").split()
+
+    # Convert the input strings to integers
+    num1, num2, num3 = map(int, numbers)
+
+    # Calculate and print the average
+    print((num1 + num2 + num3) / 3)
+# Call the average function
+average()
